ee3f14e7a4ce5465a3666e7b35fe7c5d5c4365da
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2023-02-09 21:44:30 +0000

    change style of the calculate button in order the colors are more contrasting and add the meta data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "calculate_1") {
                 calculateMonthlyInvestments();
-            }else if (this.getAttribute("data-type") === "calculate_2") {
-                calculateRetirementFundAmount(); 
+            } else if (this.getAttribute("data-type") === "calculate_2") {
+                calculateRetirementFundAmount();
             } else {
-                alert(`Unknown data tpye: ${data-type}`);
+                alert(`Unknown data type: ${data-type}`);
                 throw `Unknown data type: ${data-type}. Abborting!`;
             }
         });
     }
 
-    
-    })
+
+});
 
 
 /**
@@ -28,8 +27,8 @@ function calculateMonthlyInvestments() {
     let yearsUntilRetirement = parseInt(document.getElementById('years_until_retirement').value);
     let interestRate = parseFloat(document.getElementById('interest_rate').value);
 
-    let monthlyInvestments = Math.floor((moneyInRetirement * (interestRate/12)) / (((1+ interestRate/12)**(yearsUntilRetirement*12))-1));
-    
+    let monthlyInvestments = Math.floor((moneyInRetirement * (interestRate / 12)) / (((1 + interestRate / 12) ** (yearsUntilRetirement * 12)) - 1));
+
     let moneyInRetirementFormatted = Intl.NumberFormat().format(moneyInRetirement);
     let monthlyInvestmentsFormatted = Intl.NumberFormat().format(monthlyInvestments);
 
@@ -38,19 +37,19 @@ function calculateMonthlyInvestments() {
         have in ${yearsUntilRetirement} years ${moneyInRetirementFormatted} units of currency of your retirement fund. Check
         the next tab for more info.</p>
     `;
-    
-    
+
+
     if (yearsUntilRetirement === 1) {
         html = `
         <p>You should invest each month ${monthlyInvestmentsFormatted} units of currency, in order to
         have in ${yearsUntilRetirement} year ${moneyInRetirementFormatted} units of currency of your retirement fund. Check
         the next tab for more info.</p>
     `;
-    } 
-    
+    }
+
     let response = document.getElementById('response1');
-  
-    response.innerHTML= html;
+
+    response.innerHTML = html;
 
 }
 
@@ -60,13 +59,13 @@ function calculateMonthlyInvestments() {
  */
 
 function calculateRetirementFundAmount() {
-  
+
     let monthlyInvestments2 = parseInt(document.getElementById('monthly_investments').value);
     let yearsUntilRetirement2 = parseInt(document.getElementById('years_until_retirement_2').value);
     let interestRate2 = parseFloat(document.getElementById('interest_rate_2').value);
 
-    let moneyInRetirement2 = Math.floor(((((1 + (interestRate2/12))**(yearsUntilRetirement2 * 12))-1) / (interestRate2/12)) * monthlyInvestments2);
-    
+    let moneyInRetirement2 = Math.floor(((((1 + (interestRate2 / 12)) ** (yearsUntilRetirement2 * 12)) - 1) / (interestRate2 / 12)) * monthlyInvestments2);
+
     let moneyInRetirement2Formatted = Intl.NumberFormat().format(moneyInRetirement2);
     let monthlyInvestments2Formatted = Intl.NumberFormat().format(monthlyInvestments2);
 
@@ -85,6 +84,6 @@ function calculateRetirementFundAmount() {
     }
 
     let response2 = document.getElementById('response2');
-    response2.innerHTML= html_2;
+    response2.innerHTML = html_2;
 
-} 
+}
